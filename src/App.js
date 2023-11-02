a6a30a46d2cec01bfc1d5aedac9f4cf917ed0b2f
Valeriia14 <valeriiadesign14@gmail.com>
2023-11-02 14:56:23 +0200

    first test

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import React ,{useState , useEffect} from "react"
+
+function Timer() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+    setCount((count) => count + 1);
+  }, 1000);
+
+  return () => clearTimeout(timer)
+  }, []);
+
+  return <h1>I've rendered {count} times!</h1>;
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
